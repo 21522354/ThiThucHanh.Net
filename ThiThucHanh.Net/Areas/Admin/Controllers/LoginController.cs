fff6b72f9baf4e8ff5434e1f779570980c83f2c6
Nam Đàm Thành <21522354@ms.uit.edu.vn>
2024-10-28 15:06:15 +0700

    add SignUp

--- ThiThucHanh.Net/Areas/Admin/Controllers/LoginController.cs
+++ ThiThucHanh.Net/Areas/Admin/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
 using ThiThucHanh.Net.Models;
 
 namespace ThiThucHanh.Net.Areas.Admin.Controllers
@@ -18,6 +19,17 @@ namespace ThiThucHanh.Net.Areas.Admin.Controllers
         public IActionResult SignUp()
         {
             return View();
+        }
+        public IActionResult ConfirmSignup(string username, string password)
+        {
+            var existUser = _quanLySanPhamContext.Users.Where(p => p.Username == username).FirstOrDefault(); 
+            if(existUser != null)
+            {
+                return RedirectToAction("Index", "Product", new { Area = "" });
+            }
+            var user = new User() { Username = username, Password = password };
+            _quanLySanPhamContext.Users.Add(user);
+            return RedirectToAction("Index", "Product", new { Area = "" });
         }
         public IActionResult ConfirmLogin(string username, string password)
         {
