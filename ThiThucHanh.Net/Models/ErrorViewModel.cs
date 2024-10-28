91d1c7bc35defe6a7a5e1e3434f3913a0a10db7b
Nam Đàm Thành <21522354@ms.uit.edu.vn>
2024-10-28 09:23:35 +0700

    Add project files.

---
+++ ThiThucHanh.Net/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ThiThucHanh.Net.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
